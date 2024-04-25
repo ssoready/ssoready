b630522e96176147c521085e02d20ee013dae054
Ulysse Carion <ulyssecarion@gmail.com>
2024-04-25 13:36:17 -0700

    rename c14n tests/ to testdata/ and remove dummy test files

--- internal/c14n/c14n_test.go
+++ internal/c14n/c14n_test.go
@@ -12,7 +12,7 @@ import (
 
 func ExampleCanonicalize() {
 	input := `<foo z="2" a="1"><bar /></foo>`
-	doc, err := uxml.Parse(input)
+	doc, err := uxml.Parse([]byte(input))
 	if err != nil {
 		panic(err)
 	}
@@ -30,18 +30,18 @@ func TestCanonicalize(t *testing.T) {
 	// tests/charmods is modified from the c14n test suite. CDATA is removed, because uxml intentionally does not
 	// support CDATA. Single-quoted attributes are changed to double-quoted ones, for the same reason.
 
-	entries, err := os.ReadDir("tests")
+	entries, err := os.ReadDir("testdata")
 	assert.NoError(t, err)
 
 	for _, file := range entries {
 		t.Run(file.Name(), func(t *testing.T) {
-			in, err := os.ReadFile(fmt.Sprintf("tests/%s/in.xml", file.Name()))
+			in, err := os.ReadFile(fmt.Sprintf("testdata/%s/in.xml", file.Name()))
 			assert.NoError(t, err)
 
-			out, err := os.ReadFile(fmt.Sprintf("tests/%s/out.xml", file.Name()))
+			out, err := os.ReadFile(fmt.Sprintf("testdata/%s/out.xml", file.Name()))
 			assert.NoError(t, err)
 
-			doc, err := uxml.Parse(string(in))
+			doc, err := uxml.Parse(in)
 			assert.NoError(t, err)
 
 			actual, err := c14n.Canonicalize(doc.Root, nil)
