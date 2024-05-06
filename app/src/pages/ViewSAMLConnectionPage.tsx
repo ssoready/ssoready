e7f0f2dacdf3fa7dd5cebf0065e9f10ff9783729
Ulysse Carion <ulyssecarion@gmail.com>
2024-05-06 14:45:37 -0700

    ViewSAMLConnection: begin redesign

--- app/src/pages/ViewSAMLConnectionPage.tsx
+++ app/src/pages/ViewSAMLConnectionPage.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { useParams } from "react-router";
 import { useQuery } from "@connectrpc/connect-query";
 import {
+  getEnvironment,
   getOrganization,
   getSAMLConnection,
+  listOrganizations,
   listSAMLConnections,
   listSAMLFlows,
 } from "@/gen/ssoready/v1/ssoready-SSOReadyService_connectquery";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft } from "lucide-react";
@@ -20,103 +28,165 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import moment from "moment";
 
 export function ViewSAMLConnectionPage() {
   const { environmentId, organizationId, samlConnectionId } = useParams();
   const { data: samlConnection } = useQuery(getSAMLConnection, {
     id: samlConnectionId,
   });
+
+  return (
+    <div className="grid gap-8">
+      <Card>
+        <CardHeader>
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col space-y-1.5">
+              <div className="flex gap-4">
+                <CardTitle>SAML Connection</CardTitle>
+
+                <span className="text-xs font-mono bg-gray-100 py-1 px-2 rounded-sm">
+                  {samlConnectionId}
+                </span>
+              </div>
+
+              <CardDescription>
+                A SAML connection is a link between your product and your
+                customer's Identity Provider.
+              </CardDescription>
+            </div>
+
+            {/*{environment && (*/}
+            {/*    <EditEnvironmentAlertDialog environment={environment} />*/}
+            {/*)}*/}
+          </div>
+        </CardHeader>
+      </Card>
+
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="account">Configuration</TabsTrigger>
+          <TabsTrigger value="password">Login Flows</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Card>
+            <CardHeader>
+              <CardTitle>Service Provider Configuration</CardTitle>
+              <CardDescription>
+                The configuration here is assigned automatically by SSOReady,
+                and needs to be inputted into your customer's Identity Provider
+                by their IT admin.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid grid-cols-4 gap-y-2 items-center">
+                <div className="text-sm col-span-1 text-muted-foreground">
+                  Entity ID
+                </div>
+                <div className="text-sm col-span-3">
+                  {samlConnection?.spEntityId}
+                </div>
+
+                <div className="text-sm col-span-1 text-muted-foreground">
+                  Assertion Consumer Service (ACS) URL
+                </div>
+                <div className="text-sm col-span-3">
+                  {samlConnection?.spAcsUrl}
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card className="mt-4">
+            <CardHeader>
+              <CardTitle>Identity Provider Configuration</CardTitle>
+              <CardDescription>
+                The configuration here needs to be copied over from the
+                customer's Identity Provider.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid grid-cols-4 gap-y-2 items-center">
+                <div className="text-sm col-span-1 text-muted-foreground">
+                  Entity ID
+                </div>
+                <div className="text-sm col-span-3">
+                  {samlConnection?.idpEntityId}
+                </div>
+                <div className="text-sm col-span-1 text-muted-foreground">
+                  Redirect URL
+                </div>
+                <div className="text-sm col-span-3">
+                  {samlConnection?.idpRedirectUrl}
+                </div>
+              </div>
+
+              <Collapsible className="mt-1.5">
+                <CollapsibleTrigger className="text-sm text-muted-foreground">
+                  Certificate (click to show)
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <div className="bg-black rounded-lg px-6 py-4 mt-4">
+                    <code className="text-sm text-white">
+                      <pre>{samlConnection?.idpCertificate}</pre>
+                    </code>
+                  </div>
+                </CollapsibleContent>
+              </Collapsible>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="password">
+          <ListLoginFlowsTabContent />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
+
+function ListLoginFlowsTabContent() {
+  const { environmentId, organizationId, samlConnectionId } = useParams();
   const { data: listFlowsRes } = useQuery(listSAMLFlows, {
     samlConnectionId,
   });
 
   return (
-    <>
-      <div className="flex items-center gap-4">
-        <Button variant="outline" size="icon" className="h-7 w-7">
-          <ChevronLeft className="h-4 w-4" />
-          <span className="sr-only">Back</span>
-        </Button>
-        <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-          {samlConnectionId}
-        </h1>
-        {/*<Badge variant="outline" className="ml-auto sm:ml-0">*/}
-        {/*  In stock*/}
-        {/*</Badge>*/}
-        <div className="hidden items-center gap-2 md:ml-auto md:flex">
-          <Button size="sm" asChild>
-            <Link
-              to={`/environments/${environmentId}/organizations/${organizationId}/saml-connections/${samlConnectionId}/edit`}
-            >
-              Edit
-            </Link>
-          </Button>
-        </div>
-      </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>SAML Login Flows</CardTitle>
+        <CardDescription>
+          SAML login flows from this connection are listed here.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Timestamp</TableHead>
+              <TableHead>User ID</TableHead>
+            </TableRow>
+          </TableHeader>
 
-      <Card>
-        <CardHeader>
-          <CardTitle>{samlConnection?.id}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="font-semibold">SAML Connection Details</div>
-          <div className="flex justify-between">
-            <div>Identity Provider Entity ID</div>
-            <div>{samlConnection?.idpEntityId}</div>
-          </div>
-          <div className="flex justify-between">
-            <div>Identity Provider Redirect URL</div>
-            <div>{samlConnection?.idpRedirectUrl}</div>
-          </div>
-          <div className="flex justify-between">
-            <div>Service Provider Entity ID</div>
-            <div>{samlConnection?.spEntityId}</div>
-          </div>
-          <div className="flex justify-between">
-            <div>Service Provider ACS URL</div>
-            <div>{samlConnection?.spAcsUrl}</div>
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card>
-        <CardHeader>
-          <CardTitle>Identity Provider Certificate</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="bg-black p-6 rounded-md text-white text-sm">
-            <pre>
-              <code>{samlConnection?.idpCertificate}</code>
-            </pre>
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card>
-        <CardHeader>
-          <CardTitle>Recent SAML Login Flows</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableHead>SAML Flow ID</TableHead>
-            </TableHeader>
-            <TableBody>
-              {listFlowsRes?.samlFlows?.map((samlFlow) => (
-                <TableRow>
-                  <TableCell>
-                    <Link
-                      to={`/environments/${environmentId}/organizations/${organizationId}/saml-connections/${samlConnectionId}/flows/${samlFlow.id}`}
-                    >
-                      {samlFlow.id}
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-    </>
+          <TableBody>
+            {listFlowsRes?.samlFlows?.map((flow) => (
+              <TableRow key={flow.id}>
+                <TableCell>{flow.id}</TableCell>
+                <TableCell>
+                  {moment(flow.createTime!.toDate()).format()}
+                </TableCell>
+                <TableCell>{flow.subjectIdpId}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   );
 }
