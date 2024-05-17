d9230004df760e09effd1cd3a7e82bb1db1a7c92
Ulysse Carion <ulyssecarion@gmail.com>
2024-05-17 11:37:46 -0700

    redo HomePage

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useQuery } from "@connectrpc/connect-query";
-import { listEnvironments } from "@/gen/ssoready/v1/ssoready-SSOReadyService_connectquery";
+import {
+  getOnboardingState,
+  listEnvironments,
+} from "@/gen/ssoready/v1/ssoready-SSOReadyService_connectquery";
 import {
   Table,
   TableBody,
@@ -10,28 +13,124 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+  ArrowRight,
+  ArrowRightIcon,
+  BookIcon,
+  BookOpenTextIcon,
+  PlusIcon,
+} from "lucide-react";
 
 export function HomePage() {
+  const { data: onboardingState } = useQuery(getOnboardingState, {});
   const { data: listEnvsRes } = useQuery(listEnvironments, {});
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <div className="flex flex-col gap-y-8">
+      {onboardingState && onboardingState.onboardingEnvironmentId === "" ? (
+        <Card>
+          <CardHeader>
+            <CardTitle>Add SAML support this afternoon</CardTitle>
+            <CardDescription>
+              Our interactive onboarding flow shows you how SSOReady works, and
+              how you'll integrate it in your product.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <div className="flex items-center gap-x-4">
+              <Button asChild>
+                <Link to="/onboarding">
+                  Get started
+                  <ArrowRightIcon className="h-4 w-4 ml-2" />
+                </Link>
+              </Button>
+              <span className="text-sm text-muted-foreground">or</span>
+              <Button variant="outline" asChild>
+                <a href="https://ssoready.com/docs">
+                  <BookOpenTextIcon className="h-4 w-4 mr-2" />
+                  Read the Docs
+                </a>
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card>
+          <CardHeader>
+            <CardTitle>Welcome to SSOReady</CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            <div className="flex items-center gap-x-4">
+              <Button variant="outline" asChild>
+                <a href="https://ssoready.com/docs">
+                  <BookOpenTextIcon className="h-4 w-4 mr-2" />
+                  Read the Docs
+                </a>
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      )}
+
+      <Card>
+        <CardHeader>
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col space-y-1.5">
+              <CardTitle>Environments</CardTitle>
+
+              <CardDescription>
+                An environment corresponds to a deployment environment in your
+                product, e.g. "Staging" or "Production".
+              </CardDescription>
+            </div>
+
+            <Button variant="outline" asChild>
+              <Link to="/environments/new">
+                <PlusIcon className="h-4 w-4 mr-2" />
+                Create environment
+              </Link>
+            </Button>
+          </div>
+        </CardHeader>
+
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Environment ID</TableHead>
+                <TableHead>Display Name</TableHead>
+                <TableHead>App Redirect URL</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {listEnvsRes?.environments?.map((environment) => (
+                <TableRow key={environment.id}>
+                  <TableCell>
+                    <Link
+                      to={`/environments/${environment.id}`}
+                      className="underline underline-offset-4 decoration-muted-foreground"
+                    >
+                      {environment.id}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{environment.displayName}</TableCell>
+                  <TableCell>{environment.redirectUrl}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
