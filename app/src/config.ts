7d12646292f15bd18c1aab155bea812cb4fc7c5b
Ulysse Carion <ulysse.carion@ssoready.com>
2024-05-26 09:52:58 -0700

    Fix onboarding: use sso.dummyidp.com

--- app/src/config.ts
+++ app/src/config.ts
@@ -2,5 +2,6 @@ export const API_URL = process.env.APP_API_URL!;
 export const APP_URL = process.env.APP_APP_URL;
 export const PUBLIC_API_URL = process.env.APP_PUBLIC_API_URL!;
 export const DUMMYIDP_URL = process.env.APP_DUMMYIDP_URL!;
+export const DUMMYIDP_SSO_URL = process.env.APP_DUMMYIDP_SSO_URL!;
 export const DUMMYIDP_CERTIFICATE = process.env.APP_DUMMYIDP_CERTIFICATE!;
 export const GOOGLE_OAUTH_CLIENT_ID = process.env.APP_GOOGLE_OAUTH_CLIENT_ID!;
