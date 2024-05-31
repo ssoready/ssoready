cd2d7cb4683487f3494496197b4a4856924bbb2e
Ulysse Carion <ulysse.carion@ssoready.com>
2024-05-31 14:41:15 -0700

    app: Add Sentry

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,6 +6,22 @@ import bash from "highlight.js/lib/languages/bash";
 import xml from "highlight.js/lib/languages/xml";
 import json from "highlight.js/lib/languages/json";
 
+import * as Sentry from "@sentry/react";
+import { SENTRY_DSN, SENTRY_ENVIRONMENT } from "@/config";
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  environment: SENTRY_ENVIRONMENT,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.httpClientIntegration(),
+  ],
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  debug: true,
+});
+
 hljs.registerLanguage("bash", bash);
 hljs.registerLanguage("xml", xml);
 hljs.registerLanguage("json", json);
