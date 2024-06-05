2484fb6e4424f9e0f21345aa50d520e6eb24d637
Ulysse Carion <ulysse.carion@ssoready.com>
2024-06-05 13:05:38 -0700

    Add Segment/Posthog to app (#25)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Outlet, Route, Routes } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, Route, Routes, useLocation } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -21,6 +21,7 @@ import { API_URL } from "@/config";
 import { Toaster } from "@/components/ui/sonner";
 import { RedirectSAMLFlowPage } from "@/pages/RedirectSAMLFlowPage";
 import { OnboardingPage } from "@/pages/OnboardingPage";
+import { analytics } from "@/analytics";
 
 const queryClient = new QueryClient();
 
@@ -34,60 +35,67 @@ const transport = createConnectTransport({
   ],
 });
 
+export function AppRoutes() {
+  const location = useLocation();
+  useEffect(() => {
+    analytics.page();
+  }, [location]);
+
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/verify-email" element={<VerifyEmailPage />} />
+
+      <Route path="/" element={<LoginGate />}>
+        <Route path="/onboarding" element={<OnboardingPage />} />
+
+        <Route path="/" element={<Page />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/environments/new" element={<CreateEnvironmentPage />} />
+          <Route
+            path="/environments/:environmentId"
+            element={<ViewEnvironmentPage />}
+          />
+          <Route
+            path="/environments/:environmentId/api-keys/:apiKeyId"
+            element={<ViewAPIKeyPage />}
+          />
+          <Route
+            path="/environments/:environmentId/organizations/:organizationId"
+            element={<ViewOrganizationPage />}
+          />
+          <Route
+            path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId"
+            element={<ViewSAMLConnectionPage />}
+          />
+          <Route
+            path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId/flows"
+            element={<ViewSAMLConnectionPage />}
+          />
+          <Route
+            path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId/edit"
+            element={<EditSAMLConnectionPage />}
+          />
+          <Route
+            path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId/flows/:samlFlowId"
+            element={<ViewSAMLFlowPage />}
+          />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
 export function App() {
   return (
     <>
       <TransportProvider transport={transport}>
         <QueryClientProvider client={queryClient}>
           <BrowserRouter>
-            <Routes>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/verify-email" element={<VerifyEmailPage />} />
-
-              <Route path="/" element={<LoginGate />}>
-                <Route path="/onboarding" element={<OnboardingPage />} />
-
-                <Route path="/" element={<Page />}>
-                  <Route path="/" element={<HomePage />} />
-                  <Route
-                    path="/environments/new"
-                    element={<CreateEnvironmentPage />}
-                  />
-                  <Route
-                    path="/environments/:environmentId"
-                    element={<ViewEnvironmentPage />}
-                  />
-                  <Route
-                    path="/environments/:environmentId/api-keys/:apiKeyId"
-                    element={<ViewAPIKeyPage />}
-                  />
-                  <Route
-                    path="/environments/:environmentId/organizations/:organizationId"
-                    element={<ViewOrganizationPage />}
-                  />
-                  <Route
-                    path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId"
-                    element={<ViewSAMLConnectionPage />}
-                  />
-                  <Route
-                    path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId/flows"
-                    element={<ViewSAMLConnectionPage />}
-                  />
-                  <Route
-                    path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId/edit"
-                    element={<EditSAMLConnectionPage />}
-                  />
-                  <Route
-                    path="/environments/:environmentId/organizations/:organizationId/saml-connections/:samlConnectionId/flows/:samlFlowId"
-                    element={<ViewSAMLFlowPage />}
-                  />
-                </Route>
-              </Route>
-            </Routes>
+            <AppRoutes />
           </BrowserRouter>
         </QueryClientProvider>
       </TransportProvider>
-
       <Toaster />
     </>
   );
