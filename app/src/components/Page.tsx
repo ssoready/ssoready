4307a12d8313e085f11624602062a9a4abbd521a
Ulysse Carion <ulysse.carion@ssoready.com>
2024-06-20 16:27:56 -0700

    sidebar help (#40)

--- app/src/components/Page.tsx
+++ app/src/components/Page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Outlet, useParams } from "react-router";
 import { EnvironmentSwitcher } from "@/components/EnvironmentSwitcher";
-import { Building2, LayoutGrid, UserIcon } from "lucide-react";
+import {
+  Building2,
+  CalendarIcon,
+  LayoutGrid,
+  MailIcon,
+  PhoneIcon,
+  UserIcon,
+} from "lucide-react";
 import { Link } from "react-router-dom";
 import { useQuery } from "@connectrpc/connect-query";
 import { whoami } from "@/gen/ssoready/v1/ssoready-SSOReadyService_connectquery";
@@ -30,15 +37,54 @@ export function Page() {
           </div>
         </div>
 
-        <div className="flex items-center gap-3 border-t border-gray-200 px-4 py-4">
-          <Avatar className="h-9 w-9">
-            <AvatarFallback>
-              <UserIcon />
-            </AvatarFallback>
-          </Avatar>
-          <div className="grid gap-0.5 text-xs">
-            <div className="font-medium">{whoamiData?.displayName}</div>
-            <div className="text-gray-400">{whoamiData?.email}</div>
+        <div>
+          <div className="border-t border-gray-200 px-4 py-4">
+            <div className="flex items-center gap-3 ">
+              <div className="grid gap-0 5 text-sm">
+                <div className="font-medium">Call the SSOReady CTO</div>
+                <div className="text-gray-400 text-xs">
+                  Want to talk about SAML? You can talk to our CTO,{" "}
+                  <a className="underline" href="https://github.com/ucarion">
+                    Ulysse
+                  </a>
+                  , any time. He just loves this stuff.
+                </div>
+              </div>
+              <img
+                className="h-12 w-12 rounded-full"
+                src="/ulysse.jpg"
+                alt="CTO"
+              />
+            </div>
+
+            <div className="mt-2 text-xs text-gray-600 flex flex-col gap-y-1">
+              <div className="flex items-center">
+                <PhoneIcon className="h-4 w-4 mr-2" />
+                (510) 502 1557
+              </div>
+              <div className="flex items-center">
+                <MailIcon className="h-4 w-4 mr-2" />
+                ulysse.carion@ssoready.com
+              </div>
+              <div className="flex items-center">
+                <CalendarIcon className="h-4 w-4 mr-2" />
+                <a className="underline" href="https://cal.com/ucarion/30min">
+                  Book a meeting
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3 border-t border-gray-200 px-4 py-4">
+            <Avatar className="h-9 w-9">
+              <AvatarFallback>
+                <UserIcon />
+              </AvatarFallback>
+            </Avatar>
+            <div className="grid gap-0.5 text-xs">
+              <div className="font-medium">{whoamiData?.displayName}</div>
+              <div className="text-gray-400">{whoamiData?.email}</div>
+            </div>
           </div>
         </div>
       </div>
