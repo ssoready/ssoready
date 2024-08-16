86b16cad808c0aa6f9e16068d2dfb0423aec7683
Ravindra Nath Bura <ravindr283@gmail.com>
2024-08-17 00:31:20 +0530

    Standardize OAuth button sizes

--- app/src/pages/LoginPage.tsx
+++ app/src/pages/LoginPage.tsx
@@ -165,7 +165,7 @@ function MicrosoftLogin() {
   return (
     <Button variant="outline" asChild>
       <a
-        className="flex items-center gap-x-2"
+        className="flex items-center gap-x-[0.3rem] pl-[0.4rem] pr-[0.4rem]"
         href={microsoftRedirectUri.toString()}
       >
         <span>Continue with Microsoft</span>
