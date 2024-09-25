96dc102964e9037623969414c69c0eb19cf38485
Ulysse Carion <ulysse.carion@ssoready.com>
2024-09-25 09:56:53 -0700

    Revert "auth: simplify state query parameter signing and discontinue internal use of RelayState (#138)"
    This reverts commit 1fc87b5346c710b611e2520373df400f84a522ad.

--- internal/authservice/oauth.go
+++ internal/authservice/oauth.go
@@ -16,6 +16,7 @@ import (
 	"github.com/ssoready/ssoready/internal/authn"
 	ssoreadyv1 "github.com/ssoready/ssoready/internal/gen/ssoready/v1"
 	"github.com/ssoready/ssoready/internal/saml"
+	"github.com/ssoready/ssoready/internal/statesign"
 	"github.com/ssoready/ssoready/internal/store"
 	"github.com/ssoready/ssoready/internal/store/idformat"
 )
@@ -128,6 +129,9 @@ func (s *Service) oauthAuthorize(w http.ResponseWriter, r *http.Request) {
 	if err := acsTemplate.Execute(w, &acsTemplateData{
 		SignOnURL:   dataRes.IDPRedirectURL,
 		SAMLRequest: initRes.SAMLRequest,
+		RelayState: s.StateSigner.Encode(statesign.Data{
+			SAMLFlowID: samlFlowID,
+		}),
 	}); err != nil {
 		panic(fmt.Errorf("acsTemplate.Execute: %w", err))
 	}
